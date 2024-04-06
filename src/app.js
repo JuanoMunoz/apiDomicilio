085860be39b37a56f677de1d4acbd8da16a02a0d
Juano <juanjosemunozva@gmail.com>
2024-04-05 21:32:28 -0500

    vercel integration

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import './database.js'
 const app = express();
 const port = 3000
 import domicilioRouter from './routes/domicilio.routes.js';
 app.use(express.json());
-app.use('/api/v1',domicilioRouter)
-app.listen(port, ()=>{
+app.use('/api/v1', domicilioRouter)
+app.listen(port, () => {
     console.log('you can see the routes on localhost:3000/api/v1/domicilio')
 })
--- src/routes/domicilio.routes.js
+++ src/routes/domicilio.routes.js
@@ -1,28 +1,40 @@
 import { Router } from "express";
 import domicilio from "../model/domicilio.js";
 const domicilioRouter = Router()
-domicilioRouter.get('/domicilio', async (req,res)=>{
-    const allDomicilios = await  domicilio.find();
+domicilioRouter.get('/domicilio', async (req, res) => {
+    const allDomicilios = await domicilio.find();
     res.status(200).json({
         domicilios: allDomicilios
     })
-}).post('/domicilio', async (req,res)=>{
-    const newDomicilio = await domicilio.create(req.body);
-    res.status(201).json({
-        msg: 'domicilio created successfully',
-        domicilio: newDomicilio
+}).get('/domicilio/:id', async (req, res) => {
+    const { id } = req.params
+    const domicilioFinded = await domicilio.findOne({ idPedido: id });
+    res.status(200).json({
+        domicilio: domicilioFinded
     })
-}).delete('/domicilio/:id', async (req,res)=>{
-    const deletedDomicilio = await domicilio.findByIdAndDelete(req.params.id);
+}).post('/domicilio', async (req, res) => {
+    try {
+        const newDomicilio = await domicilio.create(req.body);
+        res.status(201).json({
+            msg: 'domicilio created successfully',
+            domicilio: newDomicilio
+        })
+    } catch (e) {
+        res.status(401).json({ errors: e })
+    }
+}).delete('/domicilio/:id', async (req, res) => {
+    const { id } = req.params
+    const deletedDomicilio = await domicilio.findOneAndDelete({ idPedido: id });
     res.status(200).json({
         msg: 'domicilio deleted successfully',
         domicilio: deletedDomicilio
     })
-}).put('/domicilio/:id',async (req,res)=>{
-    const updatedDomicilio = await domicilio.findByIdAndUpdate(req.params.id,req.body);
+}).put('/domicilio/:id', async (req, res) => {
+    const { id } = req.params
+    const findDomicilio = await domicilio.findOneAndUpdate({ idPedido: id }, req.body);
     res.status(200).json({
         msg: 'domicilio updated successfully',
-        domicilio: updatedDomicilio
+        domicilio: findDomicilio
     })
 })
 export default domicilioRouter
